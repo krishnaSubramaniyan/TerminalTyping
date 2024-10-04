6b8d7bfca73feb158f90ac0c9460e4fe813aa472
github-CS-krishna <krishnasubramaniyan2001@gmail.com>
2024-10-04 13:04:13 +0530

    calculate left and right hand load

---
+++ handload.py
@@ -0,0 +1,39 @@
+#data
+leftHand = ('a','s','d','f','g','q','w','e','r','t');
+rightHand = ('h','j','k','l',';','y','u','i','o','p');
+data = [];
+leftHandCount = 0;
+rightHandCount = 0;
+total = 0;
+
+#input
+print("enter paragraph");
+while(True):
+    temp = input();
+    if(temp == ''):
+        break
+    else:
+        data.append(temp);
+
+
+#calculate
+for i in range(len(data)):
+    for letter in data[i]:
+        if(letter == ' '):
+            rightHandCount += 1;
+        elif(letter in leftHand):
+            leftHandCount += 1;
+        elif(letter in rightHand):
+            rightHandCount += 1;
+
+total = leftHandCount + rightHandCount;
+
+print("LETTER COUNT".center(60,"="))
+print("leftHand Count  = ",leftHandCount);
+print("rightHand Count = ",rightHandCount);
+print("total           = ",total);
+
+print()
+print("PERCENTAGE".center(60,"="));
+print(f"leftHand  = {(leftHandCount/total*100):.2f}%");
+print(f"rightHand = {(rightHandCount/total*100):.2f}%");
