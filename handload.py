41df84ddaea6b6cb908bcf755ff244842a8b440a
github-CS-krishna <krishnasubramaniyan2001@gmail.com>
2024-10-23 12:53:46 +0530

    update to zxcvfmnb letters

--- handload.py
+++ handload.py
@@ -1,6 +1,6 @@
 #data
-leftHand = ('a','s','d','f','g','q','w','e','r','t');
-rightHand = ('h','j','k','l',';','y','u','i','o','p');
+leftHand = ('a','s','d','f','g','q','w','e','r','t','z','x','c','v','b');
+rightHand = ('h','j','k','l',';','y','u','i','o','p','m','n');
 data = [];
 leftHandCount = 0;
 rightHandCount = 0;
@@ -28,12 +28,12 @@ for i in range(len(data)):
 
 total = leftHandCount + rightHandCount;
 
-print("LETTER COUNT".center(60,"="))
+print("LETTER COUNT".center(60,"-"))
 print("leftHand Count  = ",leftHandCount);
 print("rightHand Count = ",rightHandCount);
 print("total           = ",total);
 
 print()
-print("PERCENTAGE".center(60,"="));
+print("PERCENTAGE".center(60,"-"));
 print(f"leftHand  = {(leftHandCount/total*100):.2f}%");
 print(f"rightHand = {(rightHandCount/total*100):.2f}%");
